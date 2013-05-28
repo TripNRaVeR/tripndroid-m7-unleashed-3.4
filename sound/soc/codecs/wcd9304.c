e936a2a9abcf64496cf57ac84aa4623731e3b2a7
Simmi Pateriya <simmip@codeaurora.org>
2013-05-28 15:58:03 +0530

    ASoC: wcd9304: Wait enough time during mbhc calibration
    If mbhc micbias has a capacitor which is large enough to delay micbias
    voltage ramp up/down, initial calibration also needs to delay enough time
    after changing mux to be able to calibrate ground and micbias voltage
    accurately.  Increase delay to 50ms during calibration and improve
    calibration accuracy.
    
    CRs-Fixed: 495526
    Change-Id: I8bf1297045565740175292a522168131754e7940
    Signed-off-by: Simmi Pateriya <simmip@codeaurora.org>

--- sound/soc/codecs/wcd9304.c
+++ sound/soc/codecs/wcd9304.c
@@ -63,6 +63,8 @@
 #define NUM_CODEC_DAIS 2
 #define SLIM_CLOSE_TIMEOUT 1000
 
+#define SITAR_MUX_SWITCH_READY_WAIT_MS 50
+
 struct sitar_codec_dai_data {
 	u32 rate;
 	u32 *ch_num;
@@ -3635,7 +3637,11 @@ void sitar_mbhc_cal(struct snd_soc_codec *codec)
 	snd_soc_write(codec, SITAR_A_CDC_MBHC_EN_CTL, 0x04);
 	snd_soc_write(codec, SITAR_A_CDC_MBHC_CLK_CTL, 0x02);
 	snd_soc_write(codec, SITAR_A_MBHC_SCALING_MUX_1, 0x81);
-	usleep_range(100, 100);
+	/*
+	* Hardware that has external cap can delay mic bias ramping down up
+	* to 50ms.
+	*/
+	msleep(SITAR_MUX_SWITCH_READY_WAIT_MS);
 	snd_soc_write(codec, SITAR_A_CDC_MBHC_EN_CTL, 0x04);
 	usleep_range(sitar->mbhc_data.t_dce, sitar->mbhc_data.t_dce);
 	sitar->mbhc_data.dce_z = sitar_codec_read_dce_result(codec);
@@ -3644,7 +3650,11 @@ void sitar_mbhc_cal(struct snd_soc_codec *codec)
 	snd_soc_write(codec, SITAR_A_CDC_MBHC_CLK_CTL, 0x0A);
 	snd_soc_write(codec, SITAR_A_CDC_MBHC_CLK_CTL, 0x02);
 	snd_soc_write(codec, SITAR_A_MBHC_SCALING_MUX_1, 0x82);
-	usleep_range(100, 100);
+	/*
+	* Hardware that has external cap can delay mic bias ramping down up
+	* to 50ms.
+	*/
+	msleep(SITAR_MUX_SWITCH_READY_WAIT_MS);
 	snd_soc_write(codec, SITAR_A_CDC_MBHC_EN_CTL, 0x04);
 	usleep_range(sitar->mbhc_data.t_dce, sitar->mbhc_data.t_dce);
 	sitar->mbhc_data.dce_mb = sitar_codec_read_dce_result(codec);
@@ -3654,14 +3664,22 @@ void sitar_mbhc_cal(struct snd_soc_codec *codec)
 	snd_soc_write(codec, SITAR_A_CDC_MBHC_EN_CTL, 0x02);
 	snd_soc_write(codec, SITAR_A_CDC_MBHC_CLK_CTL, 0x02);
 	snd_soc_write(codec, SITAR_A_MBHC_SCALING_MUX_1, 0x81);
-	usleep_range(100, 100);
+	/*
+	* Hardware that has external cap can delay mic bias ramping down up
+	* to 50ms.
+	*/
+	msleep(SITAR_MUX_SWITCH_READY_WAIT_MS);
 	snd_soc_write(codec, SITAR_A_CDC_MBHC_EN_CTL, 0x02);
 	usleep_range(sitar->mbhc_data.t_sta, sitar->mbhc_data.t_sta);
 	sitar->mbhc_data.sta_z = sitar_codec_read_sta_result(codec);
 
 	
 	snd_soc_write(codec, SITAR_A_MBHC_SCALING_MUX_1, 0x82);
-	usleep_range(100, 100);
+	/*
+	* Hardware that has external cap can delay mic bias ramping down up
+	* to 50ms.
+	*/
+	msleep(SITAR_MUX_SWITCH_READY_WAIT_MS);
 	snd_soc_write(codec, SITAR_A_CDC_MBHC_EN_CTL, 0x02);
 	usleep_range(sitar->mbhc_data.t_sta, sitar->mbhc_data.t_sta);
 	sitar->mbhc_data.sta_mb = sitar_codec_read_sta_result(codec);
@@ -3674,7 +3692,6 @@ void sitar_mbhc_cal(struct snd_soc_codec *codec)
 
 	snd_soc_write(codec, SITAR_A_MBHC_SCALING_MUX_1, 0x84);
 	usleep_range(100, 100);
-
 	wcd9xxx_enable_irq(codec->control_data, SITAR_IRQ_MBHC_POTENTIAL);
 	sitar_turn_onoff_rel_detection(codec, true);
 }
