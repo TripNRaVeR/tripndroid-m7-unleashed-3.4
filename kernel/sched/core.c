67d91b11fa35b9a184585c075bdbcf916a832f67
TripNRaVeR <noerialexeew@live.nl>
2013-07-25 01:26:19 +0200

    tdf: pre-patch 01: scheduler: compute time-average nr_running per run-queue

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2020,6 +2020,32 @@ static inline void task_group_account_field(struct task_struct *p, int index,
 #endif
 }
 
+unsigned long avg_nr_running(void)
+{
+	unsigned long i, sum = 0;
+	unsigned int seqcnt, ave_nr_running;
+
+	for_each_online_cpu(i) {
+		struct rq *q = cpu_rq(i);
+
+		/*
+		 * Update average to avoid reading stalled value if there were
+		 * no run-queue changes for a long time. On the other hand if
+		 * the changes are happening right now, just read current value
+		 * directly.
+		 */
+		seqcnt = read_seqcount_begin(&q->ave_seqcnt);
+		ave_nr_running = do_avg_nr_running(q);
+		if (read_seqcount_retry(&q->ave_seqcnt, seqcnt)) {
+			read_seqcount_begin(&q->ave_seqcnt);
+			ave_nr_running = q->ave_nr_running;
+		}
+
+		sum += ave_nr_running;
+	}
+
+	return sum;
+}
 
 void account_user_time(struct task_struct *p, cputime_t cputime,
 		       cputime_t cputime_scaled)
