51993fa4745b92d54f9214c6c809d1cefe7cc258
TripNRaVeR <noerialexeew@live.nl>
2014-01-13 23:07:24 +0100

    Revert "fix camera drivers"
    This reverts commit bd36699fb2224c9191ef0faed02cc2ef89ad73c9.

--- drivers/media/video/v4l2-device.c
+++ drivers/media/video/v4l2-device.c
@@ -180,7 +180,7 @@ int v4l2_device_register_subdev(struct v4l2_device *v4l2_dev,
 	}
 #endif
 
-#if defined(CONFIG_MACH_DELUXE_J)
+#if defined(CONFIG_MACH_DUMMY)
 	if (v4l2_dev->subdevs.prev == NULL)
 		return -EINVAL;
 #endif
